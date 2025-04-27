75f3a59f2dde41fb13f769c0d733ecd43a82d34d
Mihle <git@mihle.dev>
2025-04-27 13:15:40 +0200

    Created more functions that arent complete yet. For organization/prepping

--- src/application.py
+++ src/application.py
@@ -17,33 +17,53 @@ def range_check_int(min_int, max_int=None):
     return range_check
 
 
-parser = argparse.ArgumentParser(
-    description="Application for DRTP Reliable Transfer Protocol. Can be run as server or client "
-                "to transfer file from client to server")
-param_group = parser.add_mutually_exclusive_group(required=True)
-param_group.add_argument('-s', '--server', dest="server", action="store_true",
-                         help="Run the application in server mode, mutually exclusive with client")
-param_group.add_argument('-c', '--client', dest="server", action="store_false",
-                         help="Run the application in client mode, mutually exclusive with server")
-parser.add_argument('-i', '--ip', dest="server_address", type=str, default="127.0.0.1",
-                    help="IP address that is going to be used/is used for the server (default: 127.0.0.1)")
-parser.add_argument('-p', '--port', dest="port", type=range_check_int(1024, 65535), default=8088,
-                    help="Port that is going to be used (default: 8088)")
-parser.add_argument('-f', '--file', dest="file_name", type=str, default="",
-                    help="Name of the file that is to be transferred from client to server, does nothing for server")
-parser.add_argument('-w', '--window', dest="window", type=range_check_int(1), default=3,
-                    help="Size of the sender/receiver window for the client/server (default: 3)")
-parser.add_argument('-d', '--discard', dest="discard_packet", type=int, default=-1,
-                    help="Packet seq number of the packet that should be discarded by server, "
-                         "does nothing for client (default: -1)")
-param = parser.parse_args()
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description="Application for DRTP Reliable Transfer Protocol. Can be run as server or client "
+                    "to transfer file from client to server")
+    param_group = parser.add_mutually_exclusive_group(required=True)
+    param_group.add_argument('-s', '--server', dest="server", action="store_true",
+                             help="Run the application in server mode, mutually exclusive with client")
+    param_group.add_argument('-c', '--client', dest="client", action="store_true",
+                             help="Run the application in client mode, mutually exclusive with server")
+    parser.add_argument('-i', '--ip', dest="server_address", type=str, default="127.0.0.1",
+                        help="IP address that is going to be used/is used for the server (default: 127.0.0.1)")
+    parser.add_argument('-p', '--port', dest="port", type=range_check_int(1024, 65535), default=8088,
+                        help="Port that is going to be used (default: 8088)")
+    parser.add_argument('-f', '--file', dest="file_name", type=str, default="",
+                        help="Name of the file that is to be transferred from client to server, does nothing for server")
+    parser.add_argument('-w', '--window', dest="window", type=range_check_int(1), default=3,
+                        help="Size of the sender/receiver window for the client/server (default: 3)")
+    parser.add_argument('-d', '--discard', dest="discard_packet", type=int, default=-1,
+                        help="Packet seq number of the packet that should be discarded by server, "
+                             "does nothing for client (default: -1)")
+    return parser.parse_args()
 
 # TODO: c and s option, maybe enum og something similar
 #       Maybe different default ip address
 #       default of d should be infinity, how do you do that? negative number?
+#       Check if the dest is actually needed
+#       Add docstring/comment
 
 
 # TODO: Rest of the code
-
-# TODO: Check if you should have the _main_ thing going on in this file
 # TODO: check if you should more than one file
+
+def client(args):
+    pass
+
+
+def server(args):
+    pass
+
+
+def main():
+    args = get_arguments()
+    if server:
+        server(args)
+    elif client:
+        client(args)
+
+
+if __name__ == "__main__":
+    main()
